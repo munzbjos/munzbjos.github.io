68756d560673b1ff6d04d2ff4e1ecba5c0c74a8a
munzbjos <josef.munzberger@gmail.com>
2024-09-12 15:46:18 +0200

    js edit

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,57 +1,26 @@
-var container, stats, clock, controls;
-var camera, scene, renderer;
-init();
-animate();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function init() {
-    container = document.getElementById('container');
-    camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.set(25, 70, 50);
-    scene = new THREE.Scene();
-    scene.add(camera);
-
-    clock = new THREE.Clock();
-
-    // collada loader
-    var loader = new THREE.ColladaLoader();
-    loader.options.convertUpAxis = true;
-    loader.load("./pavilon.dae", function (collada) {
-        var avatar = collada.scene;
-        scene.add(avatar);
-    });
-
-    var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-    scene.add(ambientLight);
-
-    var directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-    directionalLight.position.set(1, 1, 1).normalize();
-    camera.add(directionalLight);
-
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setClearColor(0x000000);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-    container.appendChild(renderer.domElement);
-
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 0, 0);
-    controls.update();
-
-    window.addEventListener('resize', onWindowResize, false);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    render();
-}
-
-function render() {
-    var delta = clock.getDelta();
-    renderer.render(scene, camera);
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
