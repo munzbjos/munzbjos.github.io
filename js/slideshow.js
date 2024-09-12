10a61346227d0e2936f7d6063ab5ee2026e7d509
munzbjos <josef.munzberger@gmail.com>
2024-09-12 16:06:04 +0200

    slides

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,26 +1,25 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 1; // This starts the slideshow at the first slide
+showSlides(slideIndex); // Show the first slide when the page loads
 
+// This function increments/decrements the slide index when arrows are clicked
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
+// This function shows the slide based on the current slideIndex
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+
+  // Reset slideIndex if it's out of bounds
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
+
+  // Hide all slides
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+
+  // Show the current slide
+  slides[slideIndex - 1].style.display = "block";  
 }
